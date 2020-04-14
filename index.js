c3575590af881c5de59d01c6c655661248bb93ca
jeffzyliu <jeffzyliu@gmail.com>
2020-04-14 02:06:50 -0400

    first working version finished; may do EC later

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
 /**
- * Respond with hello worker text
- * @param {Request} request
+ * Cloudflare internship coding challenge
+ * Full Stack
+ * 
+ * Jeff Liu, April 2020
+ * 
+ * A simple Cloudflare Workers random redirection app
+ */
+
+/**
+ * default event listener for fetching this page; unchanged
+ */
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+})
+
+
+/**
+ * respond to fetch by fetching the variants JSON, then fetching a random URL
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  return fetch('https://cfw-takehome.developers.workers.dev/api/variants')
+    .then(response => response.json())
+    .then(response => {
+      return fetch(response['variants'][Math.round(Math.random())]);
+    });
 }
